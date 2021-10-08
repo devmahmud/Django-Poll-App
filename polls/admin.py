816db1746748bcbd194690aedba293ecae11e2cc
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2021-10-08 11:20:55 +0600

    update: updated django admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Poll, Choice, Vote
 
-admin.site.register(Poll)
-admin.site.register(Choice)
-admin.site.register(Vote)
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ["text", "owner", "pub_date", "active"]
+    search_fields = ["text", "owner__username"]
+    list_filter = ["active"]
+    date_hierarchy = "pub_date"
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["choice_text", "poll"]
+    search_fields = ["choice_text", "poll__text"]
+    autocomplete_fields = ["poll"]
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ["choice", "poll", "user"]
+    search_fields = ["choice__choice_text", "poll__text", "user__username"]
+    autocomplete_fields = ["choice", "poll", "user"]
