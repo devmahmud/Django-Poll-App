71a25980096adb50cdf7a0270e35392413294206
AdityaB09 <2020.aditya.bagayatkar@ves.ac.in>
2023-10-15 18:28:31 +0530

    New changes and issues resolved

--- polls/views.py
+++ polls/views.py
@@ -101,77 +101,46 @@ def polls_edit(request, poll_id):
 
     return render(request, "polls/poll_edit.html", {'form': form, 'poll': poll})
 
-
 @login_required
 def polls_delete(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
     if request.user != poll.owner:
         return redirect('home')
-    poll.delete()
+    else :
+     poll.delete()
+     all_polls = Poll.objects.all()
+    search_term = ''
+    if 'name' in request.GET:
+        all_polls = all_polls.order_by('text')
+
+    if 'date' in request.GET:
+        all_polls = all_polls.order_by('pub_date')
+
+    if 'vote' in request.GET:
+        all_polls = all_polls.annotate(Count('vote')).order_by('vote__count')
+
+    if 'search' in request.GET:
+        search_term = request.GET['search']
+        all_polls = all_polls.filter(text__icontains=search_term)
+
+
+    paginator = Paginator(all_polls, 6)  # Show 6 contacts per page
+    page = request.GET.get('page')
+    polls = paginator.get_page(page)
+
+    get_dict_copy = request.GET.copy()
+    params = get_dict_copy.pop('page', True) and get_dict_copy.urlencode()
+    print(params)
+    context = {
+        'polls': polls,
+        'params': params,
+        'search_term': search_term,
+    }
+    
     messages.success(request, "Poll Deleted successfully.",
                      extra_tags='alert alert-success alert-dismissible fade show')
-    return redirect("polls:list")
+    return render(request, 'polls/polls_list.html', context)
 
-
-@login_required
-def add_choice(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    if request.user != poll.owner:
-        return redirect('home')
-
-    if request.method == 'POST':
-        form = ChoiceAddForm(request.POST)
-        if form.is_valid:
-            new_choice = form.save(commit=False)
-            new_choice.poll = poll
-            new_choice.save()
-            messages.success(
-                request, "Choice added successfully.", extra_tags='alert alert-success alert-dismissible fade show')
-            return redirect('polls:edit', poll.id)
-    else:
-        form = ChoiceAddForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'polls/add_choice.html', context)
-
-
-@login_required
-def choice_edit(request, choice_id):
-    choice = get_object_or_404(Choice, pk=choice_id)
-    poll = get_object_or_404(Poll, pk=choice.poll.id)
-    if request.user != poll.owner:
-        return redirect('home')
-
-    if request.method == 'POST':
-        form = ChoiceAddForm(request.POST, instance=choice)
-        if form.is_valid:
-            new_choice = form.save(commit=False)
-            new_choice.poll = poll
-            new_choice.save()
-            messages.success(
-                request, "Choice Updated successfully.", extra_tags='alert alert-success alert-dismissible fade show')
-            return redirect('polls:edit', poll.id)
-    else:
-        form = ChoiceAddForm(instance=choice)
-    context = {
-        'form': form,
-        'edit_choice': True,
-        'choice': choice,
-    }
-    return render(request, 'polls/add_choice.html', context)
-
-
-@login_required
-def choice_delete(request, choice_id):
-    choice = get_object_or_404(Choice, pk=choice_id)
-    poll = get_object_or_404(Poll, pk=choice.poll.id)
-    if request.user != poll.owner:
-        return redirect('home')
-    choice.delete()
-    messages.success(
-        request, "Choice Deleted successfully.", extra_tags='alert alert-success alert-dismissible fade show')
-    return redirect('polls:edit', poll.id)
 
 
 def poll_detail(request, poll_id):
