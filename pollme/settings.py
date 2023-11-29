12b2db585f4cb0ef1f4670863834dfb46b4104df
izzyjosh <calebayuba222@gmail.com>
2023-11-29 19:05:14 +0100

    completed upgrade

--- pollme/settings.py
+++ pollme/settings.py
@@ -131,4 +131,4 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "joshuajosephizzyjosh@gmail.com"
+DEFAULT_FROM_EMAIL = "add a registered email here"
